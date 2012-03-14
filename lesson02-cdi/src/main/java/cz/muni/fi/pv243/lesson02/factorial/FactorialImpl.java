ba5cf32b4448c67103a79bda535fa1e02c044bdf
Jozef Hartinger <jharting@redhat.com>
2012-03-14 14:14:58 +0100

    Added implementation of the Factorial interface - a simple CDI bean with an injection point and scope declaration

---
+++ lesson02-cdi/src/main/java/cz/muni/fi/pv243/lesson02/factorial/FactorialImpl.java
@@ -0,0 +1,24 @@
+package cz.muni.fi.pv243.lesson02.factorial;
+
+import java.math.BigInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class FactorialImpl implements Factorial {
+
+    @Inject
+    private MathOperations math;
+
+    @Override
+    public BigInteger compute(long number) {
+        BigInteger result = null;
+        if (number == 0) {
+            result = BigInteger.ONE;
+        } else {
+            result = math.multiplySequence(1, number);
+        }
+        return result;
+    }
+}
