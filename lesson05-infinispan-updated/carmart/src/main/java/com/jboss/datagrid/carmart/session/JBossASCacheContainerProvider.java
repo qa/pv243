a981cc8637df04c28f244ec215c02a8b7583324b
Martin Gencur <mgencur@redhat.com>
2013-11-15 14:33:14 +0100

    Configure Infinispan 6 for task 1

--- lesson05-infinispan-updated/carmart/src/main/java/com/jboss/datagrid/carmart/session/JBossASCacheContainerProvider.java
+++ lesson05-infinispan-updated/carmart/src/main/java/com/jboss/datagrid/carmart/session/JBossASCacheContainerProvider.java
@@ -25,10 +25,12 @@ import java.util.logging.Logger;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import org.infinispan.commons.api.BasicCacheContainer;
+import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.eviction.EvictionStrategy;
 import org.infinispan.manager.DefaultCacheManager;
 import com.jboss.datagrid.carmart.session.CacheContainerProvider;
 
@@ -49,14 +51,16 @@ public class JBossASCacheContainerProvider implements CacheContainerProvider {
     public BasicCacheContainer getCacheContainer() {
         if (manager == null) {
 
-            
-            //*************** amend the configuration ***************
-            
-            GlobalConfiguration glob = new GlobalConfigurationBuilder().build();
-            Configuration loc = new ConfigurationBuilder().build();
-            
-            //*************** amend the configuration ***************
-            
+        	GlobalConfiguration glob = new GlobalConfigurationBuilder()
+        	                          .nonClusteredDefault().globalJmxStatistics().enable()
+        	                          .jmxDomain("org.infinispan.carmart")  //prevent collision with non-transactional carmart
+        	                          .build();
+        	Configuration loc = new ConfigurationBuilder()
+        	                        .jmxStatistics().enable()
+        	                        .clustering().cacheMode(CacheMode.LOCAL)
+        	                        .eviction().maxEntries(4).strategy(EvictionStrategy.LRU)
+        	                        .persistence().passivation(true).addSingleFileStore().purgeOnStartup(true)
+        	                        .build();
             
             manager = new DefaultCacheManager(glob, loc, true); //true means start the cache manager immediately
             log.info("=== Using DefaultCacheManager (library mode) ===");
