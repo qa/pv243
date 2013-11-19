df45e3aa1992a5e493376603cd4c98397f04f2d8
Jitka Kudrnacova <jkudrnac@redhat.com>
2013-11-19 19:17:08 +0100

    RemoteConnectionApp: draft

--- lesson07-management/src/main/java/cz/muni/fi/pv243/lesson7/RemoteConnectionApp.java
+++ lesson07-management/src/main/java/cz/muni/fi/pv243/lesson7/RemoteConnectionApp.java
@@ -21,16 +21,7 @@
  */
 package cz.muni.fi.pv243.lesson7;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.NameCallback;
-import javax.security.auth.callback.PasswordCallback;
-import javax.security.auth.callback.UnsupportedCallbackException;
-import javax.security.sasl.RealmCallback;
-import org.jboss.as.controller.client.ModelControllerClient;
-import org.jboss.dmr.ModelNode;
+
 
 /**
  * WildFly8 Java API example - connect to remote WildFly8 instance
@@ -42,44 +33,15 @@ public class RemoteConnectionApp {
 
     public static void main(String[] args) throws Exception {
 
+        // connect to remote WildFly8 instance
 
-        ModelControllerClient client = RemoteConnectionApp.createClient(
-                InetAddress.getByName("127.0.0.1"), 9999, "ferda", "mravenec1*".toCharArray(), "ManagementRealm");
+        // execute any command
 
+        // print results
 
-        ModelNode op = new ModelNode();
-        op.get("operation").set("whoami");
+        // close client
 
-        ModelNode returnVal = client.execute(op);
-
-        System.out.println(returnVal.get("result").toString());
-
-        client.close();
     }
 
-    static ModelControllerClient createClient(final InetAddress host, final int port,
-            final String username, final char[] password, final String securityRealmName) {
 
-        final CallbackHandler callbackHandler = new CallbackHandler() {
-
-            public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-                for (Callback current : callbacks) {
-                    if (current instanceof NameCallback) {
-                        NameCallback ncb = (NameCallback) current;
-                        ncb.setName(username);
-                    } else if (current instanceof PasswordCallback) {
-                        PasswordCallback pcb = (PasswordCallback) current;
-                        pcb.setPassword(password);
-                    } else if (current instanceof RealmCallback) {
-                        RealmCallback rcb = (RealmCallback) current;
-                        rcb.setText(rcb.getDefaultText());
-                    } else {
-                        throw new UnsupportedCallbackException(current);
-                    }
-                }
-            }
-        };
-
-        return ModelControllerClient.Factory.create(host, port, callbackHandler);
-    }
 }
