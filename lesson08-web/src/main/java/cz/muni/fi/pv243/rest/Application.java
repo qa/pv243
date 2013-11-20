842880deef58d1bf69d2a87af5539bb7503b9ac5
Lukas Fryc <lfryc@redhat.com>
2013-11-20 15:01:45 +0100

    exposed rest endpoints

---
+++ lesson08-web/src/main/java/cz/muni/fi/pv243/rest/Application.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pv243.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("api/v1")
+public class Application extends javax.ws.rs.core.Application {
+
+}
---
+++ lesson08-web/src/main/java/cz/muni/fi/pv243/rest/MessagesEndpoint.java
@@ -0,0 +1,33 @@
+package cz.muni.fi.pv243.rest;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import cz.muni.fi.pv243.model.Message;
+import cz.muni.fi.pv243.store.MessageStore;
+
+@Path("/messages")
+public class MessagesEndpoint {
+
+    @Inject
+    private MessageStore messages;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Message> getMessages() {
+        return messages.getMessages();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addMessage(Message message) {
+        messages.addMessage(message);
+    }
+}
